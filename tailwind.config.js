d42df4a6a9e2d39428073c824d3c4d23c1cdd6c1
Isaac <zikkyychukwudulue@gmail.com>
2024-11-05 13:07:50 +0100

    deployment changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   purge: [],
+//   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+//     darkMode: false, // or 'media' or 'class'
+//     content: [],
+//     theme: {
+//       extend: {},
+//     },
+//     variants: {
+//       extend: {
+//         display: ["group-hover"],
+//     },
+//     },
+//     plugins: [],
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    content: [],
-    theme: {
-      extend: {},
+  content: [
+    "./src/**/*.{vue,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
     },
-    variants: {
-      extend: {
-        display: ["group-hover"],
-    },
-    },
-    plugins: [],
+  },
+  plugins: [],
 }
-
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/teladoc2/', // Change this to match your GitHub repository name
   plugins: [
     vue(),
   ],
